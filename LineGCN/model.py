a6f1ab09368f96cd2a7008e73f49be15559c45e1
zhouxukun <xukun_zhou@shannonai.com>
2021-04-26 14:42:37 +0800

    完成了LGCN代码复现

---
+++ LineGCN/model.py
@@ -0,0 +1,84 @@
+import dgl.function as fn
+from torch.nn import Module, Linear
+import torch
+from torch.nn.functional import relu
+
+
+class radius_Layer(Module):
+    def __init__(self, radius):
+        super().__init__()
+        self.result = []
+        self.radius = radius
+
+    def forward(self, g, hidden):
+        g.ndata['tmp'] = hidden
+        # gather the one-hop information from graph
+        # this actions aim at adding 1 to the total combination of 2^n
+        g.update_all(fn.copy_u('tmp', 'm'), fn.sum('m', 'tmp'))
+        for i in range(self.radius - 1):  # this because zero has been applied and total power is 1+1+2+...2^n=2^(n+1)
+            for j in range(2 ** i):
+                # I thought if there could be accerlated through squraing exponentiating
+                g.update_all(fn.copy_u('tmp', 'm'), fn.sum('m', 'tmp'))
+            self.result.append(g.ndata['tmp'])
+
+
+class LGCN_Layer(Module):
+    def __init__(self, radius, input_dim, output_dim):
+        super().__init__()
+        self.prev_g_linear = Linear(input_dim, output_dim)
+        self.prev_lg_linear = Linear(input_dim, output_dim)
+        self.deg_g_linear = Linear(input_dim, output_dim)
+        self.deg_lg_linear = Linear(input_dim, output_dim)
+        self.g_fuse = Linear(input_dim, output_dim)
+        self.lg_fuse = Linear(input_dim, output_dim)
+        self.orig_radius = radius_Layer(radius)
+        self.line_radius = radius_Layer(radius)
+        self.radius = radius
+        self.g_linear = [Linear(input_dim, output_dim) for i in range(radius)]
+        self.lg_linear = [Linear(input_dim, output_dim) for i in range(radius)]
+
+    def forward(self, g, lg, g_feature, lg_feature, g_degree, lg_degree, pm_pd):
+        self.orig_radius(g, g_feature)
+        self.line_radius(lg, lg_feature)
+        g_radius_feature = [line(feature) for line, feature in zip(self.g_linear, self.orig_radius.result)]
+        lg_radius_feature = [line(feature) for line, feature in zip(self.lg_linear, self.line_radius.result)]
+        sum_g_feature = sum(g_radius_feature)
+        sum_lg_feature = sum(lg_radius_feature)
+        orig_feature = self.prev_g_linear(g_feature)
+        line_feature = self.prev_lg_linear(lg_feature)
+        g_fuse_feature = self.g_fuse(g_degree * g_feature)
+        lg_fuse_feature = self.lg_fuse(lg_degree * lg_feature)
+        g_deg_feature = self.deg_g_linear(torch.mm(pm_pd, lg_feature))
+        lg_deg_feature = self.deg_lg_linear(torch.mm(torch.transpose(pm_pd,0,1), g_feature))
+        g_feature = sum_g_feature + orig_feature + g_deg_feature + g_fuse_feature
+        lg_feature = sum_lg_feature + line_feature + lg_deg_feature + lg_fuse_feature
+        feature_size = g_feature.shape[-1]
+        result_feature = torch.cat([relu(g_feature[:feature_size // 2]), g_feature[feature_size // 2:]])
+        result_lg_feature = torch.cat([relu(lg_feature[:feature_size // 2]), lg_feature[feature_size // 2:]])
+        return result_feature, result_lg_feature
+
+
+class model(Module):
+    def __init__(self, hidden_num, input_dim, output_dim):
+        super().__init__()
+        self.layers = torch.nn.ModuleList()
+        self.layers.append(LGCN_Layer(3, input_dim, 16))
+        for i in range(hidden_num):
+            self.layers.append(LGCN_Layer(3, 16, 16))
+        self.linear = Linear(16, output_dim)
+
+    def sparse2th(self,mat):
+        value = mat.data
+        indices = torch.LongTensor([mat.row, mat.col])
+        tensor = torch.sparse.FloatTensor(indices, torch.from_numpy(value).float(), mat.shape)
+        return tensor
+    def forward(self, g, pm_pd):
+        lg = g.line_graph(backtracking=False)
+        pm_pd=self.sparse2th(pm_pd)
+        g_init = g.in_degrees().float().unsqueeze(1)
+        g_degrees = g_init
+        lg_init = lg.in_degrees().float().unsqueeze(1)
+        lg_degree = lg_init
+        for layer in self.layers:
+            g_init, lg_init = layer(g, lg, g_init, lg_init, g_degrees, lg_degree, pm_pd)
+        return self.linear(g_init)
---
+++ LineGCN/train_LGCN.py
@@ -0,0 +1,27 @@
+from numpy import mean
+from torch.nn.functional import cross_entropy
+import dgl
+import torch
+from LineGCN.model import model
+from torch.utils.data import DataLoader
+train_dataset=dgl.data.CoraBinary()
+train_set=DataLoader(train_dataset, collate_fn=train_dataset.collate_fn, drop_last=True)
+
+model=model(2,1,2)
+optim=torch.optim.Adam(model.parameters(),lr=1e-2)
+for i in range(20):
+    total_loss=[]
+    cor=0
+    acc=0
+    for [g,pmpd,label] in train_set:
+        predict=model(g,pmpd)
+        reverse_label=1-label
+        loss=min(cross_entropy(predict,torch.tensor(label)),cross_entropy(predict,torch.tensor(reverse_label)))
+        loss.backward()
+        optim.step()
+        optim.zero_grad()
+        predict=torch.argmax(predict,-1)
+        acc+=(predict==torch.tensor(label)).int().sum()
+        cor+=label.shape[0]
+        total_loss.append(loss.item())
+    print("epoch {} accuracy {} loss {}".format(i,acc/cor,mean(total_loss)))
